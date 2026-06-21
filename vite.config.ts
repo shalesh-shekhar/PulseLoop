b3cb4c5463ab627d36ffa91427788baf2d3a8fc1
shalesh-shekhar <theshaleshshekhar@gmail.com>
2026-06-21 13:41:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(() => {
   return {
+    base: process.env.GITHUB_ACTIONS ? "/PulseLoop/" : "/",
     plugins: [
       react(),
       tailwindcss(),
